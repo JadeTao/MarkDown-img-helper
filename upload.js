8495da071fbfe112b06238a99af731010e048e9c
jader_tao <jader_tao@qq.com>
2017-11-06 22:10:27 +0800

    refact with promise and ES2015 grammar

--- upload.js
+++ upload.js
@@ -1,59 +1,73 @@
 const qiniu = require("qiniu")
 const fs = require("fs")
-const async = require('async')
 
-const bucketUrl = "你的七牛云通用域名"
-const bucket = "要上传的空间名" 
+const BUCKET_URL = "" // 七牛通用域名
+const BUCKET_NAME = "" // 对象存储空间名称
+
+const PATH = `${__dirname}/file/`
 //Access Key 和 Secret Key
-qiniu.conf.ACCESS_KEY = "Access Key"
-qiniu.conf.SECRET_KEY = "Secret Key"
+qiniu.conf.ACCESS_KEY = ""
+qiniu.conf.SECRET_KEY = ""
 
 
 
+/**
+ * 构建上传策略函数
+ * @param {*} fileName 文件名
+ */
+const genUptoken = fileName => (new qiniu.rs.PutPolicy(`${BUCKET_NAME}:${fileName}`)).token()
 
-//构建上传策略函数
-const uptoken = (bucket, key) => {
-    let putPolicy = new qiniu.rs.PutPolicy(bucket + ":" + key)
-    return putPolicy.token();
-}
 
-//构造上传函数
-const uploadFile = (uptoken, key, localFile, fileUrl, index) => {
-    let extra = new qiniu.io.PutExtra();
-    qiniu.io.putFile(uptoken, key, localFile, extra, (err, ret) => {
-        if (!err) {
-            // 上传成功， 追加url至url.txt
-            fs.appendFile('./url.txt', fileUrl + '\r\n', err => {
-                if (err) {
-                    throw err
-                } else {
-                    ret.persistentId = index + 1
-                    console.log("File:" + ret.key + "\nHash:" + ret.hash + "\nid:" + ret.persistentId);
-                    console.log('has finished\n')
-                    fs.unlinkSync(localFile)
-                }
-            });
+/**
+ * 将node风格的callback转化为promise
+ * 读取目录中的文件
+ * @param {*} path 绝对路径
+ */
+const readDir = path => new Promise((resolve, reject) => {
+    fs.readdir(path, (err, files) => err ? reject(err) : resolve(files))
+})
+
+/**
+ * 将node风格的callback转化为promise
+ * 使用七牛的SDK上传文件
+ * @param {*} uptoken 
+ * @param {*} fileName 
+ * @param {*} filePath 
+ * @param {*} fileURL 
+ * @param {*} index 
+ */
+const QN_putFile = (uptoken, fileName, filePath, fileURL, index) => new Promise((resolve, reject) => {
+    const extra = new qiniu.io.PutExtra()
+    qiniu.io.putFile(uptoken, fileName, filePath, extra, (err, ret) => err ? reject(err) : resolve({ index, fileURL, ret }))
+})
+
+/**
+ * 将node风格的callback转化为promise
+ * @param {*} data 将几个参数打包，方便resolve
+ */
+const gen_result = data => new Promise((resolve, reject) => {
+    const { index, fileURL, ret } = data
+    fs.appendFile('./url.txt', fileURL + '\r\n', err => {
+        if (err) {
+            reject(err)
         } else {
-            // 上传失败， 处理返回代码
-            console.log(err)
+            ret.persistentId = index + 1
+            console.log(`File: ${ret.key}\nHash:${ret.hash}\nid:${ret.persistentId}`)
+            console.log('has finished\n')
+            resolve()
         }
     })
-}
-//程序入口
-fs.readdir(__dirname + "/file/", (err, files) => {
-    if (err) {
-        console.log(err)
-    } else {
-        console.log(files)
-        files.forEach((file, index) => {
-            //上传到七牛后保存的文件名
-            const key = file
-            const fileUrl = "你的七牛云通用域名" + file
-            //生成上传 Token
-            const token = uptoken(bucket, key)
-            //要上传文件的本地路径
-            const filePath = './file/' + file
-            uploadFile(token, key, filePath, fileUrl, index)
-        })
-    }
 })
+
+readDir(PATH).then(
+    files => files.forEach((file, index) => {
+        const fileURL = `${BUCKET_URL}/${file}`
+        const uptoken = genUptoken(file)
+        const filePath = `./file/${file}`
+        // console.log(`${fileURL}  ${uptoken}  ${filePath}`)
+        QN_putFile(uptoken, file, filePath, fileURL, index)
+            .then(gen_result, console.log)
+            .then(() => fs.unlinkSync(filePath), console.log) //删除待上传文件
+    }),
+    console.log
+)
